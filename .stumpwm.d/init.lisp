663449a6b226705786d0b08247f22a25e9ee3fb7
chsh <hyperbolicfish@outlook.com>
2025-05-11 15:24:27 +1000

    replace xterm with alacritty

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -16,10 +16,11 @@
 (define-key *root-map* (kbd "C-l") "exec slock")
 ;; Firefox
 (define-key *root-map* (kbd "b") "exec firefox")
+;; Alacritty
+(define-key *root-map* (kbd "c") "exec alacritty")
+(define-key *root-map* (kbd "C-c") "exec alacritty")
 ;; Shell
 (define-key *root-map* (kbd "n") "exec")
-;; Scrot
-(define-key *root-map* (kbd "C-s") "exec slack")
 ;; Scrot
 (define-key *root-map* (kbd "C-x") "exec flameshot gui")
 
